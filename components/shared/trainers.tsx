f0dd51408f35fa157b2b821dc35af8ca0715ac61
nikhea <imonikheaugbodaga@gmail.com>
2025-05-25 20:33:30 +0100

    wip:landing page

--- components/shared/trainers.tsx
+++ components/shared/trainers.tsx
@@ -1,16 +1,19 @@
-import { useGetTrainers } from "@/hooks/shared";
+import { useGetTrainers, useGetGymTrainer } from "@/hooks/shared";
 import { getInitials, Routes } from "@/lib";
 import Image from "next/image";
 import Link from "next/link";
 import { LoadingComponent } from "./loading";
 import { Empty } from "./empty";
+import { useGetUser } from "@/hooks/shared";
 
 interface IProps {
   showAll?: boolean;
 }
 
 export const Trainers = ({ showAll = false }: IProps) => {
-  const { data, isPending } = useGetTrainers();
+  // const { data, isPending } = useGetTrainers();
+  const { data: users, isLoading } = useGetUser();
+  const { data, isPending } = useGetGymTrainer(users?.data._id!);
   if (isPending) return <LoadingComponent />;
 
   if (data?.data.length == 0) return <Empty href={Routes["add-trainer"]} linkText="Add Trainer" desc={"Add a trainer to see list of trainers here "} src={"/members.svg"} alt={"Members Icon"} />;
@@ -20,19 +23,23 @@ export const Trainers = ({ showAll = false }: IProps) => {
   return (
     <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
       {displayedData.map((trainer, i) => (
-        <li key={i} className="flex items-center sm:items-start flex-col">
+        <li key={i} className="flex items-center sm:items-start  flex-col">
           <div className="w-full min-w-[200px] max-w-[200px] border-[7px] overflow-hidden border-brand rounded-full h-[200px] relative bg-gray-200 flex items-center justify-center">
-            {trainer.media.length > 0 ? (
+            {trainer?.media?.length > 0 ? (
               <Image fill src={trainer.media[0]} alt="Trainers" className="object-cover" />
             ) : (
               <span className="text-5xl font-semibold text-black">{getInitials(trainer.fullname)}</span>
             )}
           </div>
-          <p className="text-[#344054] mt-[30px] sm:mt-[40px] leading-[24px] font-inter font-semibold  text-xl">{trainer.fullname}</p>
-          <p className="text-[#667085] text-base font-inter">Strength training, Cardio.</p>
-          <Link className="underline text-sm text-orange-950" href={`${Routes.trainers}/${trainer._id}`}>
-            View Profile
-          </Link>
+          <div>
+            <p className="text-[#344054] mt-[30px] sm:mt-[40px] leading-[24px] font-inter font-semibold  text-xl">{trainer.fullname}</p>
+            <p className="text-[#667085] text-base font-inter ">
+              {trainer.services?.slice(0, 3).join(", ") || ""} {/* Strength training, Cardio. */}
+            </p>
+            <Link className="underline text-sm text-orange-950" href={`${Routes.trainers}/${trainer._id}`}>
+              View Profile
+            </Link>
+          </div>
         </li>
       ))}
     </ul>
