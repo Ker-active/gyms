2169eaca60f9f3c16c0e3c606d86d628087364d7
‘chidinma21’ <chidinmaonuora1@gmail.com>
2026-02-19 22:34:58 +0100

    clear pricing modal on add new pricing

--- components/shared/empty.tsx
+++ components/shared/empty.tsx
@@ -4,27 +4,30 @@ import Link from "next/link";
 
 interface IProps extends ImageProps {
   href?: Routes;
-  linkText: string;
+  linkText?: string;
   desc: string;
   imageUrl?: string;
   onClick?: () => void;
 }
 
 export const Empty = ({ href, onClick, desc, linkText, ...rest }: IProps) => {
+  const shouldRenderCTA = linkText && (href || onClick);
   return (
     <div className="flex flex-grow items-center gap-4 py-4 justify-center flex-col">
       <Image width={100} height={100} {...rest} alt="ker active" />
       <p className="text-[13px] max-w-[415px] mx-auto text-center leading-[20px] text-[#5B5971]">
         {desc}
-        {onClick ? (
-          <button onClick={onClick} className="text-primary underline">
-            {linkText}
-          </button>
-        ) : (
-          <Link className="text-primary underline" href={href!}>
-            {linkText}
-          </Link>
-        )}
+        {shouldRenderCTA &&
+          (onClick ? (
+            <button onClick={onClick} className="text-primary underline">
+              {linkText}
+            </button>
+          ) : (
+            <Link className="text-primary underline" href={href!}>
+              {linkText}
+            </Link>
+          ))}
+        {}
       </p>
     </div>
   );
